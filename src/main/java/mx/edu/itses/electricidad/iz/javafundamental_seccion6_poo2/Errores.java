290248f31b6bd31840e0d6ec7fe67574b6975824
Laczivan <l.cazl041023@itses.edu.mx>
2025-11-08 16:00:26 -0600

    Cambios

---
+++ src/main/java/mx/edu/itses/electricidad/iz/javafundamental_seccion6_poo2/Errores.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.edu.itses.electricidad.iz.javafundamental_seccion6_poo2;
+
+/**
+ *
+ * @author zunig
+ */
+public class Errores {
+    public static void main(String[] args) {
+        System.out.println("Error de sintaxis:");
+       // System.out.println("Hola mundo")
+        System.out.println("Error logico:");
+        //int a = 10;
+        //int b = 5;
+        // restar = a+b;
+        System.out.println("Exception:");
+        //int resultado = 10/0;
+    }
+}
