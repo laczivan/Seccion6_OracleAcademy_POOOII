905d0ae8c1ec512471e6ceb4aa93ec63b989a685
laczivan <l.cazl041023@itses.edu.mx>
2025-11-09 20:14:35 -0600

    Update Card.java

--- src/main/java/mx/edu/itses/electricidad/iz/javafundamental_seccion6_poo2/Card.java
+++ src/main/java/mx/edu/itses/electricidad/iz/javafundamental_seccion6_poo2/Card.java
@@ -9,5 +9,33 @@ package mx.edu.itses.electricidad.iz.javafundamental_seccion6_poo2;
  * @author zunig
  */
 public class Card {
-    
+    private String suit; 
+    private String rank; 
+    private int value;   
+
+    public Card(String suit, String rank, int value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    // Método para ajustar el valor del As de 11 a 1
+    public void setValueToOne() {
+        if (this.rank.equals("As") && this.value == 11) {
+            this.value = 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return rank + " de " + suit;
+    }
 }
